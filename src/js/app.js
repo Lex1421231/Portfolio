ad8092eed5df05e6846b40f57b143047c1e764ac
Aleksey Tsvetkov <you@example.com>
2024-08-19 22:58:13 +0300

    upp

--- src/js/app.js
+++ src/js/app.js
@@ -4,3 +4,12 @@ let titleElem = menuElem.querySelector('.title');
 titleElem.onclick = function () {
     menuElem.classList.toggle('open');
 }
+
+document.getElementById('themeToggle').addEventListener('click', function () {
+    const currentTheme = document.body.className;
+    if (currentTheme === 'light-theme') {
+        document.body.className = 'dark-theme';
+    } else {
+        document.body.className = 'light-theme';
+    }
+});
