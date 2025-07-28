91247f468a212e0b87f3358c36d60c09e6ebb42d
Aleksey Tsvetkov <lex142@inbox.ru>
2025-07-28 14:07:44 +0300

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -7,31 +7,3 @@ document.getElementById('themeToggle').addEventListener('click', function () {
     }
 });
 
-let slideIndex = 1;
-const slideChangeTime = 5000;
-const transitionDuration = 2000;
-showSlides(slideIndex);
-
-setInterval(() => {
-    plusSlides(1);
-}, 5000);
-
-function plusSlides(n) {
-    showSlides((slideIndex += n));
-}
-
-function showSlides(n) {
-    let i;
-    const slides = document.getElementsByClassName("carousel-container")[0].children;
-    if (n > slides.length) {
-        slideIndex = 1;
-    }
-    if (n < 1) {
-        slideIndex = slides.length;
-    }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[slideIndex - 1].style.display = "block";
-}
-
